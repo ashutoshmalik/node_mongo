9f50f293738f15b3d2bdf68d15ceccb967fdc543
ashutosh <ashutosh@ashutosh-VirtualBox>
2018-05-08 13:16:16 +0530

    Step 4: MongoDB read operation added

--- server/server.js
+++ server/server.js
@@ -27,17 +27,31 @@ router.use(function(req, res, next) {
 })
 
 router.route('/items').post(function(req, res) {
+	
 	var i = new item();
 	i.title = req.body.title;
 	i.price = req.body.price;
 	i.instock = req.body.instock;
 	i.photo = req.body.photo;
+	
+	console.log('Item:' + i.title);
+	
 	i.save(function (err) {
 		if(err) {
 			res.send(err);
 		}
 		res.send({message: 'Item Created !'});
 	})
+});
+
+
+router.route('/items').get(function (req, res) {
+	item.find(function (err, items) {
+		if (err) {
+			res.send(err);
+		} 
+		res.send(items);
+	});
 });
 
 console.log('REST api is running at ' + port);
